9d6a080a8b8f9631d6a74b1ef7e87d9b68e2fc4f
DmitriySurnov <79646813832@yandex.ru>
2023-11-15 19:50:23 +0300

    update

--- www/game.php
+++ www/game.php
@@ -18,11 +18,17 @@ $dices = new Dices();
 <body>
 <?php echo $dices ->getDicesValueImage(); ?>
 </body>
+<script type="module">
+    import DicesThrower from "./js/DicesThrower.js";
+    let left = () => (DicesThrower.GetLeft())
+    let  top =  () => (DicesThrower.GetTop())
+    $(".dice").css("left", left).css("top", top)
+</script>
 <footer>
     <div>
         <span> Crated by Surnov Dmitriy (2023) </span>
     </div>
 </footer>
-<script src="js/DicesThrower.js"></script>
+
 </html>
 
--- www/js/DicesThrower.js
+++ www/js/DicesThrower.js
@@ -1,10 +1,35 @@
-$(".dice").css("left", function () {
-    return (getRandomArbitrary(14,76))+"%";
-}).css("top", function () {
-    return (getRandomArbitrary(13,72))+"%";
-})
+class DicesThrower{
 
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
+    constructor() {
+    }
+    /**
+     * @public
+     * @returns {string}
+     * @method
+     */
+    GetLeft() {
+        return (this.#getRandomArbitrary(14,76))+"%";
+    }
+
+    /**
+     * @public
+     * @returns {string}
+     * @method
+     */
+    GetTop() {
+        return (this.#getRandomArbitrary(13,72))+"%";
+    }
+
+    /**
+     * @private
+     * @param min {int}
+     * @param max {int}
+     * @returns {int}
+     */
+    #getRandomArbitrary(min, max) {
+        return Math.random() * (max - min) + min;
+    }
 }
+
+export default new DicesThrower()
 
